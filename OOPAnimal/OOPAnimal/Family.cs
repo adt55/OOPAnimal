b8b42086167f47abaa44f1c63a1c4f7d73a4005f
adt55 <78002456+adt55@users.noreply.github.com>
2021-02-02 20:23:47 -0500

    First checkin

---
+++ OOPAnimal/OOPAnimal/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAnimal
+{
+    class Family
+    {
+        public string Name;
+        public int numberOfToes;
+        public int numberOfTeeth;
+        public bool whiskers;
+        public bool doesJeremyLikeIt;
+
+        public Family(string _name = null)
+        {
+            Name = _name;
+            Console.WriteLine("Creating: " + _name + " at " + DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+
+        public void setAttributes(Family sentFamily)
+        {
+            numberOfTeeth = sentFamily.numberOfTeeth;
+            numberOfToes = sentFamily.numberOfToes;
+        }
+
+        public bool doesJeremyLikeThisFamily()
+        {
+            return doesJeremyLikeIt; 
+        }
+    }
+}
---
+++ OOPAnimal/OOPAnimal/Species.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAnimal
+{
+    enum EyeColor
+    {
+        yellow, redBrown, blue
+    }
+    class Species : Genus
+    {
+        public string binomialName;
+        public int numberOfLegs;
+        public List<EyeColor> eyeColor;
+        public Species()
+        {
+            Name = "No name";
+        }
+        public Species(string _name)
+        {
+            if (_name is null || _name.Length < 1)
+            {
+                Name = "No name";
+            }
+            else
+            {
+                Name = _name.Replace(";","");
+            }
+            Console.WriteLine("Creating: " + _name + " at " + DateTime.Now.ToString("yyyy-MM-dd"));
+
+        }
+        public Species(string _name, string _binomialName) : this(_name)
+        {
+            if (_binomialName is null || _binomialName.Length < 1)
+            {
+                _binomialName = "No name";
+            }
+            else
+            {
+                _binomialName = _name.Replace(";", "");
+            }
+        }
+        public Species(string _name, int _numberofLegs) : this(_name)
+        {
+            _numberofLegs = _numberofLegs;
+        
+        }
+
+    } 
+    
+}
